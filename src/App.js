c4b0c86eaa99a61f14f59c58d05200aacfee0ea7
vaishali <vaishalirdj14@gmail.com>
2023-10-01 12:32:09 +0530

    Requirements pages added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./LandingPage/Landing";
 import Navbar from "./LandingPage/components/Navbar";
+import Requirements from "./RescuePortal/pages/Requirements";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />}></Route>
+        {/* <Route path="/" element={<Landing />}></Route> */}
       </Routes>
+      <Requirements/>
     </BrowserRouter>
   );
 }
