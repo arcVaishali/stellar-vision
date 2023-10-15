05f1d2cab8d1e630acbccf2f6325a7d0b096247e
priyanshu tiwari <priyanshu108tiwari@gmail.com>
2023-10-15 20:00:38 +0530

    added responsiveness in landing-page

--- src/LandingPage/Landing.js
+++ src/LandingPage/Landing.js
@@ -19,6 +19,7 @@ const Landing = () => {
             <img
               src="https://i.ibb.co/S6dS0Gw/relief-connect-logo-removebg-preview.png"
               className="w-[90px]"
+              alt="logo"
             />
           </div>
           {navigation.map((element) => (
@@ -48,11 +49,12 @@ const Landing = () => {
           // src="https://images.unsplash.com/photo-1602980068989-cb21869ab2c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d2lsZGZpcmUlMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"
           src="https://plus.unsplash.com/premium_photo-1661962514590-7cfa4f3d446d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zmxvb2RzJTIwaW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60"
           className="col-span-12 inset-0 h-[100vh] w-full object-fill object-right z-0 md:object-center"
+          alt="bird in the sky"
         />
         <div className="bg-black opacity-40 absolute inset-0 z-10"></div>
-        <div className="flex flex-col col-span-9 col-start-2 absolute mx-auto max-w-7xl z-20">
+        <div className=" flex flex-col col-span-9 col-start-2 absolute mx-auto max-w-7xl z-20">
           <span
-            className="text-7xl text-gray-300 uppercase"
+            className="text-5xl md:text-7xl text-gray-300 uppercase"
             style={{
               fontFamily: "'Paytone One', sans-serif",
               textShadow: "5px 5px 5px rgba(0, 0, 0, 1)",
@@ -60,7 +62,7 @@ const Landing = () => {
           >
             Relief Connect
           </span>
-          <span className="text-xl mt-4 text-gray-400">
+          <span className="text-xl w-full lg:w-full my-4 text-gray-400">
             Our mission at Relief Connect is simple yet profound: to empower and
             uplift flood-affected individuals and communities. We are dedicated
             to providing immediate aid, including food, shelter, and rescue
@@ -72,14 +74,14 @@ const Landing = () => {
             unity, and unwavering commitment to rebuilding lives in the face of
             adversity.
           </span>
-          <div className="flex  z-20">
-            <div className="px-2 mr-4 py-2 my-2 border-4 border-white bg-transparent w-64  text-white text-center hover:bg-white hover:border-gray-300 hover:shadow-md hover:shadow-gray-500 hover:text-inherit">
+          <div className="flex   z-20  ">
+            <div className="px-2 mr-4 py-2 my-2 border-4 border-white bg-transparent md:w-64  text-white text-center hover:bg-white hover:border-gray-300 hover:shadow-md hover:shadow-gray-500 hover:text-inherit">
               <Link
                 to="/as-responder"
                 className="flex items-center justify-center uppercase"
               >
                 Get started as Responder
-                <i class="ri-arrow-right-fill text-2xl"></i>
+                <i class="ri-arrow-right-fill text-2xl "></i>
               </Link>
             </div>
             <div className="px-2 py-2 my-2 border-4 border-white bg-transparent w-64  text-white text-center hover:bg-white hover:border-gray-300 hover:shadow-md hover:shadow-gray-500 hover:text-inherit">
